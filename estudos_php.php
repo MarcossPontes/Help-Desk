b20a54be9d1341d28a3d834b424228d767091584
Marcos Pontes <marcoss.pontes@hotmail.com>
2021-07-22 17:46:53 -0300

    Create estudos_php.php

---
+++ estudos_php.php
@@ -0,0 +1,17 @@
+<?php
+
+    function multiplicarValores($valor1, $valor2) {
+        $v1 = $valor1;
+        $v2 = $valor2; 
+        $resultado = $v1 * $v2;
+
+        return $resultado;
+    }
+
+    $primeiro = 369;
+    $segundo = 4560218;
+
+    echo multiplicarValores($primeiro, $segundo);
+
+
+?>
